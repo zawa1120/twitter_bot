7316465d622ce1bcb72b05455f873538119c39a2
zawa1120 <ckz1120@gmail.com>
2021-01-10 23:40:45 +0900

    fix_bot

--- functions/ClashRoyale/deploy.sh
+++ functions/ClashRoyale/deploy.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy twitter_bot --region us-central1 --entry-point main --memory 30MB --runtime python37 --timeout 540s --env-vars-file env.yaml --trigger-topic twitter_bot
+gcloud functions deploy twitter_bot --region us-central1 --entry-point main --runtime python37 --timeout 540s  --trigger-topic twitter_bot
--- functions/ClashRoyale/main.py
+++ functions/ClashRoyale/main.py
@@ -1,1 +1,11 @@
-print("OK")
+import base64
+import datetime
+import json
+import os
+import re
+import math
+
+now = datetime.datetime.now()
+
+def main(now):
+    print(now)
