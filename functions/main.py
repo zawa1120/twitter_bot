d4753d9292852471c166618f39455e31bb59b53d
zawa1120 <ckz1120@gmail.com>
2021-02-08 23:17:44 +0900

    fix_bot

--- functions/main.py
+++ functions/main.py
@@ -2,16 +2,17 @@ import json
 import os
 import base64
 
-import tweepy
+import requests_oauthlib
 
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_key_secret = os.environ.get('CONSUMER_KEY_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
+URL = 'https://api.twitter.com/1.1/statuses/update.json'
+
 def auth_keys(CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
-    return auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return OAuth1Session(CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 def main(event, context):
     '''Triggered from a message on a Cloud Pub/Sub topic.
@@ -23,9 +24,10 @@ def main(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     if pubsub_message:
         text = pubsub_message
-
+        
     else:
-        text = "楽天紹介コード\\JX8hyMeQ9vhi\\お互いポイント貰えます♪"
+        text = "楽天モバイル紹介コード\n\nJX8hyMeQ9vhi\n\n楽天ポイントが1000pt貰えます♪\n\n#楽天モバイル\n#楽天モバイル紹介コード"
 
-    api = auth_keys(consumer_key, consumer_key_secret, access_token, access_token_secret)
-    api.update_status(text)
+    params = {"status": text }
+    twitter = auth_keys(consumer_key, consumer_key_secret, access_token, access_token_secret)
+    twitter.post(URL, params = params)
