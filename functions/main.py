3b607c5deaddb852a14f15092f597ace263a326a
zawa1120 <ckz1120@gmail.com>
2021-02-09 10:31:33 +0900

    fix_bot

--- functions/main.py
+++ functions/main.py
@@ -17,18 +17,7 @@ def auth_keys(CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECR
     return OAuth1Session(CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 def main(event, context):
-    '''Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    '''
-
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    if pubsub_message != project_name:
-        text = pubsub_message
-        
-    else:
-        text = "楽天モバイル紹介コード\n\nJX8hyMeQ9vhi\n\n楽天ポイントが1000pt貰えます♪\n\n#楽天モバイル\n#楽天モバイル紹介コード"
+    text = "楽天モバイル紹介コード\n\nJX8hyMeQ9vhi\n\n楽天ポイントが1000pt貰えます♪\n\n#楽天モバイル\n#楽天モバイル紹介コード"
 
     params = {"status": text}
     twitter = auth_keys(consumer_key, consumer_key_secret, access_token, access_token_secret)
